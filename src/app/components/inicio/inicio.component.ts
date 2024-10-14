9e5838af91410c8a33063e437a2eaba1324fe91c
Aida Garmer <aidarile@gmail.com>
2024-10-14 21:17:33 +0200

    modificaciones varias

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
+  imagenes:  { imagen: string; ruta: string; } [] = [
+    {
+      imagen: "/images/valorant.jpg",
+      ruta: '/agentes'
+    },
+    {
+      imagen: "/images/monsterhunter.jpg",
+      ruta: '/armas'
+    },
+    {
+      imagen: "/images/pokemoncards.jpg",
+      ruta: '/cartas'
+    },
+    {
+      imagen: "/images/finalspace.jpeg",
+      ruta: '/personajes'
+    }    
+    
+  ]
 
 }
